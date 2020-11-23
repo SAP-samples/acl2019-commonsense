6e3c12784d38ba818cbf84ada23df9ec9bfad3ee
Tassilo Klein <tassilo.klein@sap.com>
2020-11-23 17:33:16 +0100

    updated license

--- commonsense.py
+++ commonsense.py
@@ -1,3 +1,9 @@
+#
+# SPDX-FileCopyrightText: 2019-2020 SAP SE or an SAP affiliate company
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 import sys
 #sys.path.append("/home/ubuntu/bertviz/")
 from bertviz.pytorch_pretrained_bert import BertModel, BertTokenizer 
--- data_processors.py
+++ data_processors.py
@@ -1,3 +1,9 @@
+#
+# SPDX-FileCopyrightText: 2019-2020 SAP SE or an SAP affiliate company
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 import xml.etree.ElementTree
 import os
 import nltk
